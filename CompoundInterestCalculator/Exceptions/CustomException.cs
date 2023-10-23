b4f9eba73149cfcb3ce598f7aadbfb7642379da6
jmpompeo <joeypompeo@gmail.com>
2023-10-22 22:46:53 -0400

    added error handling and custom exceptions. not sure if using custom exceptions yet.

---
+++ CompoundInterestCalculator/Exceptions/CustomException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompoundInterestCalculator.Exceptions
+{
+    public class CustomException : Exception
+    {
+        public List<Exception>? Exceptions { get; set; }
+
+        public CustomException(List<Exception> exceptions) : base("Multiple exceptions may have occured")
+        {
+            Exceptions = exceptions;
+        }
+    }
+}
