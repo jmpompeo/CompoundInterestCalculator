07681159d03e70639c31261c5189afb92786bbf1
jmpompeo <joeypompeo@gmail.com>
2024-03-19 23:01:08 -0400

    added function app & formatted code

--- CompoundCalc/Function1.cs
+++ CompoundCalc/Function1.cs
@@ -15,7 +15,8 @@ namespace CompoundCalc
         }
 
         [Function("Function1")]
-        public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+        public IActionResult Run([HttpTrigger(AuthorizationLevel.Function,
+            "get", "post")] HttpRequest req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
             return new OkObjectResult("Welcome to Azure Functions!");
