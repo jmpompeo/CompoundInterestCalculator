28020f9a302c0f1c43fe0febe0494f0c8ff12de7
jmpompeo <joeypompeo@gmail.com>
2024-03-25 16:16:04 -0400

    added request validation tests

---
+++ CompoundInterestCalculatorTests/Models/Requests/InterestCalcReqTests.cs
@@ -0,0 +1,32 @@
+using CompoundCalc.Models.Requests;
+using Xunit;
+
+namespace CompoundInterestCalculatorTests.Models.Requests;
+
+public class IntereestCalcReqTests
+{
+    [Fact]
+    public void InterestCalcReq_Should_Throw_Out_Of_Range_When_Starting_Balance_Is_Negative()
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() => new InterestCalcReq(-100, 5, 5));
+    }
+
+    [Fact]
+    public void InterestCalcReq_Should_Throw_Out_Of_Range_When_Interest_Rate_Is_Negative()
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() => new InterestCalcReq(100, -5, 5));
+    }
+
+    [Fact]
+    public void InterestCalcReq_Should_Throw_Out_Of_Range_When_Years_Negative()
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() => new InterestCalcReq(100, 5, -5));
+    }
+
+    [Fact]
+    public void InterestCalcReq_Should_Throw_Out_Of_Range_When_Years_Over_100()
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() => new InterestCalcReq(100, 5, 105));
+    }
+
+}
