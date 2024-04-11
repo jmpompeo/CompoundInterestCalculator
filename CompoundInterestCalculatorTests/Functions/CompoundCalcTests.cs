a1eca50df5b5f0c60670d7c204e9d3751a566961
jmpompeo <joeypompeo@gmail.com>
2024-04-10 20:15:08 -0400

    fixed bug in yaml file to deploy function code

---
+++ CompoundInterestCalculatorTests/Functions/CompoundCalcTests.cs
@@ -0,0 +1,30 @@
+﻿using CompoundCalc.Functions;
+using CompoundCalc.Models.Requests;
+using CompoundCalc.Services;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+
+namespace CompoundInterestCalculatorTests.Functions;
+
+public class CompoundCalcTests
+{
+    private readonly Mock<CalculationService> _mockCalcService;
+    private readonly Mock<ILogger<CompoundCalculator>> _mockLogger = new();
+    private readonly CompoundCalculator _sut;
+
+    public CompoundCalcTests()
+    {
+        _mockCalcService = new Mock<CalculationService>();
+        _sut = new CompoundCalculator(_mockLogger.Object, _mockCalcService.Object);
+    }
+
+    [Fact]
+    public async Task CompoundCalculator_Returns_Ok_Object_Result()
+    {
+        var req = new InterestCalcReq(1000, 10, 10);
+
+        //var result = 
+
+    }
+}
