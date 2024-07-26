426af1de38b89653b84b061216b406fe6b3923d5
Prockentine <prockentine@gmail.com>
2024-07-25 21:40:08 -0400

    created next.js project

---
+++ compound-interest-calculator/src/app/layout.tsx
@@ -0,0 +1,14 @@
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>
+        {/* Layout UI */}
+        <main></main>{children}<main/>
+      </body>
+    </html>
+  )
+}
---
+++ compound-interest-calculator/src/app/page.tsx
@@ -0,0 +1,8 @@
+const HomePage = () => (
+  <div>
+    <h2>Home Page</h2>
+    <p>Welcome to the home page</p>
+  </div>
+);
+
+export default HomePage;
